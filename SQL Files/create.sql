5b73ad410acc61b7ad3d9c14affac89a91993637
Shawn Hernan <shawnhernan@hotmail.com>
2021-02-06 14:07:58 -0800

    sql create file

--- SQL Files/create.sql
+++ SQL Files/create.sql
@@ -1,1 +1,23 @@
 -- This is a test
+USE [Rosie]
+GO
+
+/****** Object:  Table [dbo].[People]    Script Date: 2/6/2021 2:06:27 PM ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[People]') AND type in (N'U'))
+DROP TABLE [dbo].[People]
+GO
+
+/****** Object:  Table [dbo].[People]    Script Date: 2/6/2021 2:06:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[People](
+	[Name] [text] NOT NULL,
+	[SSN] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
